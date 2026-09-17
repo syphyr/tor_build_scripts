3543173e234262b9f3bee307d2bb014df3483142
syphyr <syphyr@gmail.com>
2026-09-17 22:30:09 +0200

    Set build flags when building Tor

--- build_tor.sh
+++ build_tor.sh
@@ -1,4 +1,19 @@
-export PKG_CONFIG_PATH="$(pwd)/libs/build_libs/lib/pkgconfig"
+# Base path
+export TOR_LIBS_PATH="$(pwd)/libs/build_libs"
+
+# Package configuration
+export PKG_CONFIG_PATH="${TOR_LIBS_PATH}/lib/pkgconfig"
+
+# LZMA settings
+export LZMA_CFLAGS="-I${TOR_LIBS_PATH}/include -DLZMA_API_STATIC"
+export LZMA_LIBS="-L${TOR_LIBS_PATH}/lib -llzma -pthread"
+
+# ZSTD settings
+export ZSTD_CFLAGS="-I${TOR_LIBS_PATH}/include"
+export ZSTD_LIBS="-L${TOR_LIBS_PATH}/lib -lzstd -pthread"
+
+# Tell the linker where to find libraries at runtime
+export LD_LIBRARY_PATH=${TOR_LIBS_PATH}/lib:${LD_LIBRARY_PATH}
 
 sudo update-alternatives --set gcc /usr/bin/gcc-8
 sudo update-alternatives --set g++ /usr/bin/g++-8
